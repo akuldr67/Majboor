7735725fedcd4f94421c268a595d12b5e1de4528
Ayush333 <aayushmeghwani@gmail.com>
2019-04-06 01:44:21 +0530

    First push with intro slides

---
+++ app/src/main/java/com/example/medhelp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.medhelp;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PrefManager prefManager = new PrefManager(getApplicationContext());
+        if(prefManager.isFirstTimeLaunch()){
+            prefManager.setFirstTimeLaunch(false);
+            startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+            finish();
+        }
+    }
+    protected void btn_Click(View view){
+        Toast.makeText(MainActivity.this, "clicked on button", Toast.LENGTH_LONG).show();
+    }
+}
